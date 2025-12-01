32ad4ab60aedeea9b641ec29b0dc3bf15e6e2a89
Lee-Jang-Hwan <dlwkdghks0807@gmail.com>
2025-12-01 16:36:22 +0900

    구글애널리틱스 태그 설정정

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,21 @@ export default function RootLayout({
     <ClerkProvider localization={koKR}>
       <html lang="ko" suppressHydrationWarning>
         <head>
+          {/* Google Analytics */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `}
+          </Script>
+
+          {/* Google Tag Manager */}
           <Script id="google-tag-manager" strategy="afterInteractive">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
